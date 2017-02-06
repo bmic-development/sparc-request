332ea13ccb1752a9657b3b0aa331558d0a7c490c
Kyle Glick <glickk@musc.edu>
2017-02-06 09:48:45 -0500

    adding audit back to associated_survey

--- app/models/associated_survey.rb
+++ app/models/associated_survey.rb
@@ -19,6 +19,8 @@
 # TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 class AssociatedSurvey < ActiveRecord::Base
+  audited
+  
   belongs_to :survey
   belongs_to :surveyable, polymorphic: true
   
