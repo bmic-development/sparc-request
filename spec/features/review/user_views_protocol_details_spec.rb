34134837e1aa07d8e2830a2c47a8c9a99e454997
BigChiefSmidgeums <maruhntt@g.cofc.edu>
2017-03-06 13:48:19 -0500

    Fixed feature specs

--- spec/features/review/user_views_protocol_details_spec.rb
+++ spec/features/review/user_views_protocol_details_spec.rb
@@ -42,7 +42,7 @@ RSpec.describe 'User views a SSR', js: true do
       visit review_service_request_path(@sr)
       wait_for_javascript_to_finish
 
-      click_button 'View Study Details'
+      click_link 'View Study Details'
       wait_for_javascript_to_finish
 
       expect(page).to have_selector('.modal-dialog', text: "Study Details: ##{@protocol.id}", visible: true)
